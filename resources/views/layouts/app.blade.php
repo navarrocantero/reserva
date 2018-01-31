25516ff5f59cd322a4e313a2e207d8ea6977505c
navarrocantero <navarrocantero@navarrocanteros-iMac.local>
2018-01-31 04:18:14 +0100

    add house asyn task

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,23 +4,9 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+@include('layouts.style')
 
-
-    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"
-          integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-
-
-    <link rel="stylesheet"
-          href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css"
-          integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy"
-          crossorigin="anonymous">
-
-    <link rel="stylesheet" href="{{asset('css/reserva.css')}}">
-
-
-    <!-- CSRF Token -->
+<!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
@@ -151,27 +137,14 @@
 </div>
 
 {{--<div id="footer">--}}
-    {{--<div id="footer-element" class="card-footer">--}}
-        {{--<i class="fa fa-twitter" aria-hidden="true"></i>--}}
-        {{--<i class="fa fa-facebook-square" aria-hidden="true"></i>--}}
-        {{--<i class="fa fa-linkedin-square" aria-hidden="true"></i>--}}
+{{--<div id="footer-element" class="card-footer">--}}
+{{--<i class="fa fa-twitter" aria-hidden="true"></i>--}}
+{{--<i class="fa fa-facebook-square" aria-hidden="true"></i>--}}
+{{--<i class="fa fa-linkedin-square" aria-hidden="true"></i>--}}
 
-    {{--</div>--}}
 {{--</div>--}}
-
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
-        crossorigin="anonymous"></script>
-
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
-        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
-        crossorigin="anonymous"></script>
-
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js"
-        integrity="sha384-a5N7Y/aK3qNeh15eJKGWxsqtnX/wWdSZSKp+81YjTmS15nvnvxKHuzaWwXHDli+4"
-        crossorigin="anonymous"></script>
-
-<script src="{{ asset('js/reserva.js') }}"></script>
+{{--</div>--}}
+@include('layouts.scripts')
 
 </body>
 </html>
